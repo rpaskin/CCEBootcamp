18ab8f17020994b5a3d35f120016d6655aad4863
Ronnie Paskin <ronnie@megaputz.com>
2018-01-24 21:30:06 -0200

    Atualizado

--- ex1_otimizar.sol
+++ ex1_otimizar.sol
@@ -1,43 +1,114 @@
 pragma solidity ^0.4.0;
 
-contract StorageGasChallenge {
+contract StorageGasChallenge 
+{
     uint storedData;
-
-    function set(uint x) public {
+    uint[] employees;
+    function set(uint x) public 
+    {
         storedData = x;
     }
 
-    function get() public constant returns (uint) {
+    function addEmployee(uint id)
+    {
+        employees.push(id);
+    }
+    
+    function validEmployee(uint id, uint[] list) public returns(bool)
+    {
+        for(uint i = 0; i < list.length; i++)
+        {
+            if(list[i]==id)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    function get() public constant returns (uint) 
+    {
         return storedData;
     }
 
-    function increaseStorageBy(int value) public{
-      for(int i = 0; i < value; i++){
+    function increaseStorageBy(uint value) public
+    {
+      for(uint i = 0; i < value; i++)
+      {
         storedData += 1;
       }
     }
 
-    function decreaseStorageBy(int value) public{
-      for(int i = 0; i < value; i++){
+    function decreaseStorageBy(uint value) public
+    {
+      for(uint i = 0; i < value; i++)
+      {
         storedData -= 1;
       }
     }
 
-    function isOdd(uint256 num) public returns (bool){
-      if (num == 2**256 - 1){
+    function isOdd(uint256 num) public returns (bool)
+    {
+      if (num == 2**256 - 1)
+      {
         return true;
-      } else if (num == 0){
+      }
+      else if (num == 0)
+      {
         return false;
       }
 
       return isOdd(num + 2);
     }
 
-    function isGreaterThanStoredData(uint256 num) public returns (bool){
-        if(num > storedData){
+    function isGreaterThanStoredData(uint256 num) public returns (bool)
+    {
+        if(num > storedData)
+        {
             return true;
-        }else{
+        }
+        else
+        {
             return false;
         }
+    }
+    
+    
+    
+    function isDataPrime() public view returns (bool) 
+    {
+        uint num = 2;
+        
+        while ( num < storedData )
+        {
+            if ( storedData % num == 0 )
+            {
+                return false;
+            }
+            
+            num = num + 1;
+        }
+        return true;
+    }
+    
+    function findTheClosestPowerofTwoToStoreData() public view returns (uint)
+    {
+        uint power = 1;
+        uint num = 2;
+        
+        while ( num < storedData )
+        {
+            uint iterations = num ;
+            while ( iterations > 0 )
+            {
+                num = num + 1;
+                iterations = iterations - 1;
+            }
+            
+            power = power + 1;
+        }
+        
+        return power;
     }
 }
