d4903b21a77692058b71b701d83fef050255e193
jptbrandao <brandao@Joaos-MacBook-Pro-2.local>
2018-01-26 19:46:05 -0200

    bug fixes

--- simpleToken.sol
+++ simpleToken.sol
@@ -28,37 +28,5 @@ contract SimpleToken
       balance[to] += amount;
     }
 }
-// Versão do solidity
-pragma solidity ^0.4.0;
 
-contract SimpleToken
-{
-    uint supply;
-    mapping ( address => uint ) balance;
-
-    // O que é um modifier?
-    modifier onlyOwner()
-    {
-      require(msg.sender == owner);
-
-      // O que é esse símbolo?
-      _ ;
-    }
-
-    // Construtor
-    function SimpleToken(uint _supply) public
-    {
-      supply = _supply;
-    }
-
-    // Funcao transfer
-    function transferTo(address to, uint amount) public onlyOwner
-    {
-      // Considerar overflow
-      require( balance[to] + amount > balance[to]);
-
-      balance[msg.sender] -= amount;
-      balance[to] += amount;
-    }
-}
 
