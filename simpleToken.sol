49ec844b432f107b95bb7e47f440e261bd3e3eda
jptbrandao <brandao@Joaos-MacBook-Pro-2.local>
2018-01-26 19:44:33 -0200

    bug fixes

--- simpleToken.sol
+++ simpleToken.sol
@@ -6,6 +6,36 @@ contract SimpleToken
     uint supply;
     mapping ( address => uint ) balance;
 
+    // Construtor
+    function SimpleToken(uint _supply) public
+    {
+      supply = _supply;
+      balance[msg.sender] = supply;
+    }
+
+    function getBalance() public returns (uint)
+    {
+      return balance[msg.sender];
+    }
+
+    // Funcao transfer
+    function transferTo(address to, uint amount) public
+    {
+      // Considerar overflow
+      require( balance[to] + amount > balance[to]);
+
+      balance[msg.sender] -= amount;
+      balance[to] += amount;
+    }
+}
+// Versão do solidity
+pragma solidity ^0.4.0;
+
+contract SimpleToken
+{
+    uint supply;
+    mapping ( address => uint ) balance;
+
     // O que é um modifier?
     modifier onlyOwner()
     {
